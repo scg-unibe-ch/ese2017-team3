f93e4e25f20d05f8faaecf38a34e18141761ff7d
Dave Meier <dave.meier2016@gmail.com>
2017-10-12 20:31:12 +0200

    Das Projekt "ese2017-full-integration-master" hinzugefügt unter dem Namen "initial"

---
+++ initial/src/main/java/ch/unibe/scg/ese2017/fullintegration/Application.java
@@ -0,0 +1,13 @@
+package ch.unibe.scg.ese2017.fullintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
