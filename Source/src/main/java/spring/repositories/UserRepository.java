9dc42e300057c75128e05ca143b365bb55925c97
MadCatZz <julian.weyermann@students.unibe.ch>
2017-10-22 23:31:13 +0200

    functional security with roles and privileges

--- Source/src/main/java/spring/repositories/UserRepository.java
+++ Source/src/main/java/spring/repositories/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 import spring.entity.User;
 
 @Repository
-public interface  UserRepository extends CrudRepository<User, Long>{
+public interface UserRepository extends CrudRepository<User, Long> {
 
     User findByUserName(String username);
+}
