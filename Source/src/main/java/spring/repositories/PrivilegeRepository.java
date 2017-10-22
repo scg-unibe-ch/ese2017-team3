2ac22ecc2c7e11c79ebc8118c13392428810b610
Icewater1337 <fuchsmath@gmail.com>
2017-10-22 23:23:12 +0200

    fixed some stuff

--- Source/src/main/java/spring/repositories/PrivilegeRepository.java
+++ Source/src/main/java/spring/repositories/PrivilegeRepository.java
@@ -1,11 +1,11 @@
-//package spring.repositories;
-//
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//import spring.entity.Privilege;
-//
-//@Repository
-//public interface PrivilegeRepository extends CrudRepository<Privilege, Long > {
-//
-//    Privilege findByName(String privilegeName);
-//}
+package spring.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import spring.entity.Privilege;
+
+@Repository
+public interface PrivilegeRepository extends CrudRepository<Privilege, Long > {
+
+    Privilege findByName(String privilegeName);
+}
