99daabb8b15e71e90ceaaa9afe879bc6e21f5d97
Luca Rolshoven <luca.rolshoven@students.unibe.ch>
2017-11-13 00:13:24 +0100

    Error in address fields now get added to the binding result when creating or changing a tour. Tours won't be changed, when there is an error in some address field. However, the error message does not still work for address fields.

--- Source/src/main/java/spring/entity/Tour.java
+++ Source/src/main/java/spring/entity/Tour.java
@@ -3,6 +3,7 @@ package spring.entity;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -27,9 +28,11 @@ public class Tour {
     private int numberOfAnimals;
 
     @OneToOne
+    @Valid
     private Address startAddress;
 
     @OneToOne
+    @Valid
     private Address destinationAddress;
 
     @NotNull(message = "Please specify a start date for the tour.")
