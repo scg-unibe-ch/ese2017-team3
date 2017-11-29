5dc6e777570e6905f8591da8a730f9859269d57b
Luca Rolshoven <luca.rolshoven@students.unibe.ch>
2017-11-29 21:59:52 +0100

    TourRepositoryAndEntityTests now works as expected again. schema.sql was renamed to users_authorities.sql and now only gets executed during the execution of the application, not during tests. For tests there is a separate SQL file.

--- Source/src/test/java/spring/TourRepositoryAndEntityTest.java
+++ Source/src/test/java/spring/TourRepositoryAndEntityTest.java
@@ -11,7 +11,10 @@ import spring.entity.Address;
 import spring.entity.Driver;
 import spring.entity.Tour;
 import spring.entity.Truck;
+import spring.repositories.AddressRepository;
+import spring.repositories.DriverRepository;
 import spring.repositories.TourRepository;
+import spring.repositories.TruckRepository;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -25,39 +28,59 @@ public class TourRepositoryAndEntityTest {
     private Tour tour = new Tour();
     private Truck truck = new Truck();
     private Driver driver = new Driver();
+    private Address driverAddress = new Address();
     private Address startAddress = new Address();
     private Address endAddress = new Address();
 
     @Autowired
     private TourRepository tourRepository;
 
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @Autowired
+    private TruckRepository truckRepository;
+
+    @Autowired
+    private DriverRepository driverRepository;
+
     @Before
     public void setUp() {
-        driver.setUsername("Alfi");
-        driver.setId(1);
-        driver.setHiringDate(LocalDate.now());
-        driver.getAddress().setLastname("Alfredo");
-        driver.getAddress().setFirstname("Gorgonzola");
 
-        truck.setAvailable(true);
-        truck.setId(1);
-        truck.setTruckType("BIG");
+        driverAddress.setCity("Zürich");
+        driverAddress.setLastname("Alfredo");
+        driverAddress.setFirstname("Gorgonzola");
+        driverAddress.setZip(2222);
+        driverAddress.setStreet("Bergstrasse");
+        driverAddress.setStreetNumber("100");
 
-        startAddress.setId(1);
         startAddress.setCity("Narnia");
-        startAddress.setLastname("Gowen");
-        startAddress.setFirstname("Geter");
+        startAddress.setFirstname("Gowen");
+        startAddress.setLastname("Geter");
         startAddress.setZip(1001);
         startAddress.setStreet("Rhubarb Hill");
         startAddress.setStreetNumber("12A");
 
-        endAddress.setId(1);
         endAddress.setCity("Bielefeld");
-        endAddress.setLastname("Sam");
-        endAddress.setFirstname("Sung");
+        endAddress.setFirstname("Sam");
+        endAddress.setLastname("Sung");
         endAddress.setZip(1001);
         endAddress.setStreet("Galaxy Avenue");
         endAddress.setStreetNumber("8E");
+
+        addressRepository.save(driverAddress);
+        addressRepository.save(startAddress);
+        addressRepository.save(endAddress);
+
+        driver.setUsername("Alfi");
+        driver.setHiringDate(LocalDate.now());
+        driver.setAddress(driverAddress);
+
+        driverRepository.save(driver);
+
+        truck.setAvailable(true);
+        truck.setTruckType("BIG");
+        truckRepository.save(truck);
 
         tour.setCargo("Monkey");
         tour.setDriver(driver.getUsername());
@@ -66,16 +89,16 @@ public class TourRepositoryAndEntityTest {
         tour.setDeliveryStartDate(LocalDate.now());
         tour.setDeliveryStartTime(LocalTime.now());
         tour.setTruck(truck);
-//        tour.setStartAddress(startAddress);
-//        tour.setDestinationAddress(endAddress);
+        tour.setStartAddress(startAddress);
+        tour.setDestinationAddress(endAddress);
         tour.setTimeFrame("05:00 - 05:02");
+        tourRepository.save(tour);
     }
 
     @Test
     public void saveAndRetrieve() {
-        tourRepository.save(tour);
         Tour found = tourRepository.findOne(new Long(1));
-        assertTrue(found.equals(tour));
+        assertEquals(found, tour);
     }
 
 }
